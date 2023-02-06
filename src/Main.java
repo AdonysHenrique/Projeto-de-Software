772f97cf1213cdd6423a8dd5c30d371ab02f8372
Adonys Henrique <adonyshenriquevs@gmail.com>
2023-02-06 03:32:54 -0300

    Mudança

--- src/Main.java
+++ src/Main.java
@@ -5,9 +5,13 @@ public class Main {
 
         Usuario User1 = new Usuario();
         Scanner Leitor = new Scanner(System.in);
+        System.out.println("Digite seu nome:");
         String Usuario = Leitor.nextLine();
+        System.out.println("Digite seu CPF:");
         String Cpf = Leitor.nextLine();
+        System.out.println("Digite seu Email:");
         String Email = Leitor.nextLine();
+        System.out.println("Digite seu numero:");
         String Numero = Leitor.nextLine();
         User1.criarConta(Usuario,Cpf, Email, Numero);
         User1.estadoAtual();
