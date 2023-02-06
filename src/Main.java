424dd11b5c2e19b80db62253f045e79477c324d5
Adonys Henrique <adonyshenriquevs@gmail.com>
2023-02-06 02:57:21 -0300

    Primeiro commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Usuario User1 = new Usuario();
+        User1.criarConta("Adonys","131.218.894-43", "ahvs@ic.ufal.br", "82 98165-0211");
+        User1.estadoAtual();
+    }
+}
---
+++ src/Usuario.java
@@ -0,0 +1,68 @@
+public class Usuario {
+
+    public Usuario() {
+        this.setStatus(false);
+    }
+
+    public String nome;
+    public String cpf;
+    public String email;
+    public String numero;
+    public boolean status;
+    public void estadoAtual(){
+        System.out.println("-----------------");
+        System.out.println("Nome: " + this.getNome());
+        System.out.println("CPF: " + this.getCpf());
+        System.out.println("Email: " + this.getEmail());
+        System.out.println("Numero: " + this.getNumero());
+        System.out.println("Status da conta: " + this.isStatus());
+    }
+
+    public void criarConta(String nome, String cpf, String email, String numero){
+        setNome(nome);
+        setCpf(cpf);
+        setEmail(email);
+        setNumero(numero);
+        setStatus(true);
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getNumero() {
+        return String.valueOf(numero);
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    public boolean isStatus() {
+        return status;
+    }
+
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
+}
